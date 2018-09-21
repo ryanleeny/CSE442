1841c678b8da494591723385062301c416599846
mengchunhsieh <mengchun@buffalo.edu>
2018-09-21 14:13:44 -0400

    Button Class Created and Orgnized code

--- mainUI.py
+++ mainUI.py
@@ -2,14 +2,59 @@ import pygame
 import tkinter.messagebox
 
 
+class button():
+    def button(text, x, y, width, height, inactive_color, active_color, action = None):
+        cur = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x + width > cur[0] > x and y + height > cur[1] > y:
+            pygame.draw.rect(window, active_color, (x,y,width,height))
+            if click[0] == 1 and action != None:
+                if action == "setting":
+                    print("Setting Button Clicked")
+
+                if action == "play":
+                    print("Play Button Click, go to gameLoop")
+                    game.gameLoop()
+
+                if action == "quit":
+                    pygame.quit()
+                    quit()
+        else:
+            pygame.draw.rect(window, inactive_color, (x,y,width,height))
+        button.text_to_button(text,black,x,y,width,height)
+
+    def text_to_button(msg, color, buttonx, buttony, buttonwidth, buttonheight, size = "small"):
+        textSurf, textRect = button.text_objects(msg,color,size)
+        textRect.center = ((buttonx+(buttonwidth/2)), buttony+(buttonheight/2))
+        window.blit(textSurf, textRect)
+
+    def text_objects(text, color,size = "small"):
+        smallfont = pygame.font.SysFont("comicsansms", 25)
+        medfont = pygame.font.SysFont("comicsansms", 50)
+        argefont = pygame.font.SysFont("comicsansms", 85)
+        if size == "small":
+            textSurface = smallfont.render(text, True, color)
+        if size == "medium":
+            textSurface = medfont.render(text, True, color)
+        if size == "large":
+            textSurface = largefont.render(text, True, color)
+        return textSurface, textSurface.get_rect()
+
+
+class game():
+    def gameLoop():
+        gameExit = False
+        gameOver = False
+        while not gameExit:
+            print ("Game Playing")
+
 class main(object):
     ####global functions here#####
     global text_objects, galaxy_background
+    global window
 
     #####global varaible declaration here#####
     global black,white,green,red,gold,blue,game_score,background,width,height,x,y,cursor1,Fire_In_The_Hole
-    global Score_Button,Setting_Button
-
     black = (0, 0, 0)
     white = (255, 255, 255)
     green = (0, 255, 0)
@@ -21,8 +66,10 @@ class main(object):
     game_score = 0#score to display in the screen
     background = pygame.image.load('background.jpeg')
     cursor1 =pygame.image.load('cursor1.png') #cant find any good cursor so i use miku, if you guys find one repalce it
+    white = (255,255,255)
+    black = (0,0,0)
 
-##################True mean the button is being pressed##############
+    ####True mean the button is being pressed####
     Fire_In_The_Hole = False
     Score_Button = False
     Setting_Button = False
@@ -31,22 +78,19 @@ class main(object):
     x=width*1 #x and y is the position of background img
     y=height*1
 
-
-
     #####The font of button object#####
     def text_objects(text, font):
         textSurface = font.render(text, True, gold)
         return textSurface, textSurface.get_rect()
 
-
     ####Below initialize the GUI (Before LOOP)#####
     def __init__(self):
-        pygame.init()
-        window = pygame.display.set_mode((width, height))
-        pygame.display.set_caption("Galaga")
-        clock =pygame.time.Clock()
-
-
+        red = (200,0,0)
+        light_red = (255,0,0)
+        yellow = (200,200,0)
+        light_yellow = (255,255,0)
+        green = (34,177,76)
+        light_green = (0,255,0)
 
         ####main loop of the GUI#####
         gameLoop = True  # ;This the the loop of the main game, FALSE to exit the loop
@@ -61,55 +105,26 @@ class main(object):
             x=mouse[0]*-0.05
             y=mouse[1]*-0.05
             window.blit(background, (x, y))
-            pygame.draw.rect(window, white, (150, 350, 100, 50))#;;;;Draw rec Left Button
-            pygame.draw.rect(window, white, (150, 550, 100, 50))#;;;;Draw rec Right Button
-            pygame.draw.rect(window, white, (150, 450, 100, 50))# ;;;;Draw rec last Button
-            ###################mouse##############
+
+            #####mouse######
             mx=mouse[0]-cursor1.get_width()/2
             my=mouse[1]-cursor1.get_height()/2
             pygame.mouse.set_visible(False)
+
             #####Making button change color by matching the button location#####
             mouse = pygame.mouse.get_pos()
-            #print(mouse)#;printing the mouse position
-            #######Add Mouse press detection########
+       
+            #####Add Mouse press detection######
             mousepress =pygame.mouse.get_pressed()
 
             global Fire_In_The_Hole, Score_Button,Setting_Button
             Fire_In_The_Hole = False## Button press reset##
-            pygame.draw.rect(window, red, (150, 350, 100, 50))  # ;Default color of left button
-            if 150 + 100 > mouse[0] > 150 and 350 + 50 > mouse[1] > 350:
-                if mousepress[0]==True:
-                    Fire_In_The_Hole =True
-                    print (Fire_In_The_Hole)
-                    pygame.draw.rect(window, white, (150, 350, 100, 50))###color when mouse is inside left button
 
-            Setting_Button = False
-            pygame.draw.rect(window, blue, (150, 550, 100, 50))  ###Default color of right button
-            if 150 + 100 > mouse[0] > 150 and 550 + 50 > mouse[1] > 550:
-                if mousepress[0] == True:
-                    Setting_Button = True
-                    pygame.draw.rect(window, white, (150, 550, 100, 50))###color when mosue is inside right button
+            ####Create Button####
+            button.button("play", 150, 350, 100, 50, green, (0,255,0), action="play")
+            button.button("setting", 150, 450, 100, 50, yellow, (255,255,0), action="setting")
+            button.button("quit", 150, 550, 100, 50, red, (255,0,0), action ="quit")
 
-            Score_Button = False
-            pygame.draw.rect(window, green, (150, 450, 100, 50))
-            if 150 + 100 > mouse[0] > 150 and 450 + 50 > mouse[1] > 450:
-                if mousepress[0]==True:
-                    Score_Button = True
-                    pygame.draw.rect(window, white, (150, 450, 100, 50))###color when mouse is inside left button
-
-
-
-            ####WRITE TO THE BUTTONS#####
-            buttontext=pygame.font.Font("chela-one/ChelaOne-Regular.ttf",13)
-            textSurf, Leftbutton = text_objects("Fire In The Hole", buttontext)
-            Leftbutton.center = ((150 + (100 / 2)), (350 + (50 / 2)))
-            window.blit(textSurf, Leftbutton)
-            textSurf, Rightbutton = text_objects("Setting", buttontext)
-            Rightbutton.center = ((150 + (100 / 2)), (550 + (50 / 2)))
-            window.blit(textSurf, Rightbutton)
-            textSurf, lastbutton = text_objects("Score", buttontext)
-            lastbutton.center = ((150 + (100 / 2)), (450 + (50 / 2)))
-            window.blit(textSurf, lastbutton)
 
             ####coins_Display#####
             Score_text = pygame.font.Font("chela-one/ChelaOne-Regular.ttf", 40)#creating font object
@@ -121,31 +136,13 @@ class main(object):
             window.blit(cursor1, (mx, my))
             #####refresh everything#####
             clock.tick(60)
-            print(Fire_In_The_Hole)
+            # print(Fire_In_The_Hole)
             pygame.display.update()
-
-            ####Example how to use the mouse press Varaible#####
-            if Fire_In_The_Hole==True:
-                root=tkinter.Tk()
-                root.withdraw()
-                tkinter.messagebox.showwarning('Warning', 'warning you pressed an empty button',)
-
-            if Score_Button == True:
-                root = tkinter.Tk()
-                root.withdraw()
-                tkinter.messagebox.showwarning('Error', 'developer is too broke to implement this function', )
-
-            if Setting_Button == True:
-                root = tkinter.Tk()
-                root.withdraw()
-                tkinter.messagebox.showinfo('info', 'Change your setting in your own devices', )
-
-
-
 pygame.quit()
 
-
-
 if __name__ == '__main__':
+    pygame.init()
+    window = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Galaga")
+    clock =pygame.time.Clock()
     game = main()
-
