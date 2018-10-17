da9262ac617e2dece9208bc551f20892ebbb2a1e
zhijieli <jeffrey.zj.li@gmail.com>
2018-10-17 14:35:44 -0400

    fixed timer bug with weapon class

--- weapon.py
+++ weapon.py
@@ -7,7 +7,7 @@ import weapon
 longshot =[pygame.image.load("images/longshot/shot_1.png"),pygame.image.load("images/longshot/shot_2.png"),pygame.image.load("images/longshot/shot_3.png"),pygame.image.load("images/longshot/shot_4.png"),pygame.image.load("images/longshot/shot_5.png"),pygame.image.load("images/longshot/shot_6.png"),pygame.image.load("images/longshot/shot_7.png")]
 bullet1 = [pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet2.png"),pygame.image.load("images/bullet2.png"),pygame.image.load("images/bullet2.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet2.png"),]
 rocket = [pygame.image.load("images/rocket/shot_1.png"),pygame.image.load("images/rocket/shot_2.png"),pygame.image.load("images/rocket/shot_3.png"),pygame.image.load("images/rocket/shot_4.png"),pygame.image.load("images/rocket/shot_5.png"),pygame.image.load("images/rocket/shot_6.png"),pygame.image.load("images/rocket/shot_7.png")]
-weapon_frequency = [500, 1000,60]  #set how fast the bullet come out of the player in ms
+weapon_frequency = [500, 1000, 60]  #set how fast the bullet come out of the player in ms
 
 #initialzation of bullets
 class Weapon(pygame.sprite.Sprite):
@@ -16,20 +16,21 @@ class Weapon(pygame.sprite.Sprite):
     weapon_choice = 1   # which weapon are choosen, index of the weapons array
     weapon_img = [bullet1,rocket,longshot] #weapons array
     weapon_speed = [-10, -5,0] #how fast the bullets are moving, the entry index are associated to the same index of the weapons array
-    animation_speed = [3, 15,-1] # how much pixls the bullets move up every refresh, here the bigger negative the faster bullets move
+    animation_speed = [3, 15,1] # how much pixls the bullets move up every refresh, here the bigger negative the faster bullets move
 
     def __init__(self,x,y):
 
         pygame.sprite.Sprite.__init__(self)
         self.image = self.weapon_img[self.weapon_choice][0]
         self.rect = self.image.get_rect()
-        self.rect.bottom = y
+        self.rect.bottom = y - 15
         self.rect.centerx = x
         self.speedy = self.weapon_speed[self.weapon_choice]
         self.frequency = weapon_frequency[self.weapon_choice]
         self.frequency_count = self.frequency
         self.WEAPON_CHOICE = self.weapon_choice;
         self.mask = pygame.mask.from_surface(self.image)
+        self.image =pygame.Surface((100,100))
         # survival
         self.survival = True
 
