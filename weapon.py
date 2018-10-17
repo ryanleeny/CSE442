885c7565039e3a0dfad070bd0f369b9d5f21fda6
zhijieli <jeffrey.zj.li@gmail.com>
2018-10-16 20:05:38 -0400

    added commet to the weapon class

--- weapon.py
+++ weapon.py
@@ -3,19 +3,20 @@ import Hero
 from os import path
 import weapon
 
+# load the bullets image into array
 longshot =[pygame.image.load("images/longshot/shot_1.png"),pygame.image.load("images/longshot/shot_2.png"),pygame.image.load("images/longshot/shot_3.png"),pygame.image.load("images/longshot/shot_4.png"),pygame.image.load("images/longshot/shot_5.png"),pygame.image.load("images/longshot/shot_6.png"),pygame.image.load("images/longshot/shot_7.png")]
 bullet1 = [pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet2.png"),pygame.image.load("images/bullet2.png"),pygame.image.load("images/bullet2.png"),pygame.image.load("images/bullet1.png"),pygame.image.load("images/bullet2.png"),]
 rocket = [pygame.image.load("images/rocket/shot_1.png"),pygame.image.load("images/rocket/shot_2.png"),pygame.image.load("images/rocket/shot_3.png"),pygame.image.load("images/rocket/shot_4.png"),pygame.image.load("images/rocket/shot_5.png"),pygame.image.load("images/rocket/shot_6.png"),pygame.image.load("images/rocket/shot_7.png")]
-weapon_frequency = [500, 1000,60]
+weapon_frequency = [500, 1000,60]  #set how fast the bullet come out of the player in ms
 
-
+#initialzation of bullets
 class Weapon(pygame.sprite.Sprite):
-    animate = 1
-    path = [0,animate**4]
-    weapon_choice = 1
-    weapon_img = [bullet1,rocket,longshot]
-    weapon_speed = [-10, -5,0]
-    animation_speed = [3, 15,-1]
+    animate = 1   # begining index of the array
+    path = [0,animate**4]  #not use
+    weapon_choice = 1   # which weapon are choosen, index of the weapons array
+    weapon_img = [bullet1,rocket,longshot] #weapons array
+    weapon_speed = [-10, -5,0] #how fast the bullets are moving, the entry index are associated to the same index of the weapons array
+    animation_speed = [3, 15,-1] # how much pixls the bullets move up every refresh, here the bigger negative the faster bullets move
 
     def __init__(self,x,y):
 
@@ -31,15 +32,18 @@ class Weapon(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         # survival
         self.survival = True
+
+     #this update the index in the weapon image array, but with modification of incrementing speed
     def check_animate(self):
             self.animate+=1
             if (len(self.weapon_img[self.WEAPON_CHOICE])*self.animation_speed[self.WEAPON_CHOICE] <= self.animate):
                 self.animate = 0
 
 
-
+     #update the next image display of the weapon array, the index are pointed by the variable self.animate above
     def change_img(self):
         self.image = self.weapon_img[self.WEAPON_CHOICE][self.animate//self.animation_speed[self.WEAPON_CHOICE]]
+    #update the bullets class
     def update(self):
 
         #if(self.WEAPON_CHOICE==2):
